761b37a3fb8503d2ce9261cd9de2c3bddc9fc435
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-17 09:00:59 +0530

    lambda layer construct added

---
+++ infra/cdk/stack_blueprints/lambda_layer_construct.py
@@ -0,0 +1,26 @@
+"""Code for generating lambda layers."""
+from typing import List
+import aws_cdk
+import aws_cdk.aws_lambda as _lambda
+
+
+class LambdaLayerConstruct:
+    """Construct for creating lambdas layers."""
+    
+    @staticmethod
+    def create_lambda_layer(
+        stack: aws_cdk.Stack,
+        env: str,
+        config: dict,
+        layer_name: str,
+        compatible_runtimes: List[_lambda.Runtime]
+    ) -> _lambda.LayerVersion:
+        """Method to create Lambda Layers."""
+        
+        code_location = _lambda.AssetCode(config['global'][f"{layer_name}_location"])
+        return _lambda.LayerVersion(
+            scope=stack,
+            id=f"{config['global']['app-name']}-{layer_name}-Id",
+            code=code_location,
+            compatible_runtimes=compatible_runtimes
+        )
